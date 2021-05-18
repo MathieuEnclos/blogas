29b7d00c388d75b05fe210ec04a8d0aef2deb579
EugenieKa <eugenie.karadjoff@gmail.com>
2021-05-18 21:02:27 +0200

    MAJ BDD

--- src/conf/blog.sql
+++ src/conf/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 18 mai 2021 à 20:52
+-- Généré le : mar. 18 mai 2021 à 21:01
 -- Version du serveur :  8.0.25-0ubuntu0.20.04.1
 -- Version de PHP : 7.4.3
 
@@ -21,6 +21,122 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `blog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `billets`
+--
+
+CREATE TABLE `billets` (
+  `id` int NOT NULL,
+  `titre` varchar(64) DEFAULT NULL,
+  `body` text,
+  `cat_id` int DEFAULT '1',
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `billets`
+--
+
+INSERT INTO `billets` (`id`, `titre`, `body`, `cat_id`, `date`) VALUES
+(1, 'go sluc, go', 'tout est dans le titre', 1, '2014-11-20'),
+(2, 'Concert : nolwenn live', 'c\'est d\'la balle, Ca vaut bien Mick Jagger et Iggy Stooges réunis.\r\nAngus Young doit l\'ecouter en boucle...', 3, '2014-11-20'),
+(3, 'Titanic', 'c\'est l\'histoire d\'un gros bateau qui croise un glaçon', 2, '2014-11-20');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
+  `titre` varchar(64) NOT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `titre`, `description`) VALUES
+(1, 'sport', 'tout sur le sport en general'),
+(2, 'cinema', 'tout sur le cinema'),
+(3, 'music', 'toute la music que j\'aaiiiimeuh, elle vient de la, elle vient du bluuuuuuzee'),
+(4, 'tele', 'tout sur les programmes tele, les emissions, les series, et vos stars preferes'),
+(8, 'test', 'catégorie de test'),
+(9, 'test', 'catégorie de test'),
+(10, 'test', 'catégorie de test');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membres`
+--
+
+CREATE TABLE `membres` (
+  `id` int NOT NULL,
+  `pseudo` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `nom` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `prenom` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `hash` varchar(62) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `membres`
+--
+
+INSERT INTO `membres` (`id`, `pseudo`, `nom`, `prenom`, `email`, `hash`, `admin`) VALUES
+(1, 'Kintana', 'Kintana', 'Evgeniya', 'evgeniya.kintana@gmail.com', '$2y$10$UHEVb3RkGy48GkFIP1q2L.vB7luS4TFBAgP0HD.4qme4W0N9ZhsdS', 0),
+(2, 'Darkos92', 'Pivert', 'Augustin', 'augustin.pivert@free.fr', '$2y$10$RwLHKWoMRsmRZunrvfi.8OMRnCIqAtmYMnlHlhoZypHAVMQIMBmDG', 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `billets`
+--
+ALTER TABLE `billets`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `membres`
+--
+ALTER TABLE `membres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `billets`
+--
+ALTER TABLE `billets`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `membres`
+--
+ALTER TABLE `membres`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
